6eeda20d9cb0ccfe1ecc3e29c783ab6cee0c645f
lop-cz <lop@lop.cz>
2013-08-28 18:49:34 +0200

    Include SWF module

--- Gruntfile.js
+++ Gruntfile.js
@@ -72,6 +72,7 @@ module.exports = function(grunt) {
           'iframe',
           'gallery',
           'retina',
+          'swf',
           'fastclick'
         ],
         basePath: 'src/js/',
