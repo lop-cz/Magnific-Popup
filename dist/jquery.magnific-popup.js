54f1fd5f54d9f1c8e5eed5692f83583af73932de
lop-cz <lop@lop.cz>
2013-08-29 02:01:02 +0200

    SWF module included

--- dist/jquery.magnific-popup.js
+++ dist/jquery.magnific-popup.js
@@ -1,4 +1,4 @@
-/*! Magnific Popup - v0.9.5 - 2013-08-21
+/*! Magnific Popup - v0.9.5 - 2013-08-29
 * http://dimsemenov.com/plugins/magnific-popup/
 * Copyright (c) 2013 Dmitry Semenov; */
 ;(function($) {
@@ -1926,6 +1926,81 @@ $.magnificPopup.registerModule(RETINA_NS, {
 });
 
 /*>>retina*/
+
+/*>>swf*/
+
+var SWF_NS = 'swf',
+	_replaceMarkupIE = function(markup, values) {
+		var arr;
+		$.each(values, function(key, value) {
+			if(value === undefined || value === false) {
+				return true;
+			}
+			arr = key.split('_');
+			markup = markup.replace('%'+ arr[0] +'%', value);
+		});
+		return markup;
+	};
+
+$.magnificPopup.registerModule(SWF_NS, {
+	options: {
+		markup: '<div class="mfp-swf-scaler">'+
+				'<div class="mfp-close"></div>'+
+				'<object id="mfp-swf-object" class="mfp-swf" type="application/x-shockwave-flash" data="" width="100%" height="100%">'+
+					'<param name="allowfullscreen" value="true" />'+
+					'<param name="allowscriptaccess" value="always" />'+
+					'<param class="mfp-wmode" name="wmode" value="opaque" />'+
+					'<p class="mfp-fallback"></p>'+
+				'</object>'+
+			'</div>',
+		markupIE: '<div class="mfp-swf-scaler">'+
+				'<div class="mfp-close"></div>'+
+				'<object id="mfp-swf-object" class="mfp-swf" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="100%" height="100%">'+
+					'<param name="movie" value="%movie%" />'+
+					'<param name="allowfullscreen" value="true" />'+
+					'<param name="allowscriptaccess" value="always" />'+
+					'<param name="wmode" value="%wmode%" />'+
+					'<p class="mfp-fallback">%fallback%</p>'+
+				'</object>'+
+			'</div>',
+		wmode: 'opaque',
+		fallback: 'This page requires <a href="http://get.adobe.com/flashplayer/">Adobe Flash Player</a>'
+	},
+	proto: {
+		initSwf: function() {
+			mfp.types.push(SWF_NS);
+			mfp.isIE = navigator.appVersion.indexOf("MSIE") !== -1; // TODO: watch out for different UA string in IE11
+
+			_mfpOn('BeforeOpen', function(e) {
+					if (mfp.isIE) mfp.st[SWF_NS].markup = mfp.st[SWF_NS].markupIE;
+			});
+		},
+		getSwf: function(item, template) {
+			var embedSrc = item.src;
+			var swfSt = mfp.st.swf;
+			
+			var dataObj = {
+				wmode_value: swfSt.wmode,
+				fallback: swfSt.fallback
+			};
+			if (mfp.isIE) {
+				dataObj.movie_value = embedSrc;	// 'movie' param for IE
+				// Replace values in markup string and recreate <object> in DOM
+				// (IE BUG) -> elements under <object> cannot be simply selected and modified
+				var markup = _replaceMarkupIE(swfSt.markup, dataObj);
+				template = mfp.currTemplate[SWF_NS] = $(markup);
+			} else {
+				dataObj.swf_data = embedSrc;
+			}
+			mfp._parseMarkup(template, dataObj, item);
+			mfp.updateStatus('ready');
+			return template;
+		}
+	}
+});
+
+
+/*>>swf*/
 
 /*>>fastclick*/
 /**
